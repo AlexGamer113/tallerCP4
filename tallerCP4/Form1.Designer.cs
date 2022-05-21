3324a82c65c10fe65b34b9c3e9d35d4b114bbefe
AlexGamer113 <eduardoalex1013@gmail.com>
2022-05-21 00:52:04 -0600

    Se sube

---
+++ tallerCP4/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace tallerCP4
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInicio));
+            this.picBanner = new System.Windows.Forms.PictureBox();
+            this.pnlTextos = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCuentasGoogle = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.picUsername = new System.Windows.Forms.PictureBox();
+            this.picPassword = new System.Windows.Forms.PictureBox();
+            this.chkUsername = new System.Windows.Forms.CheckBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBanner)).BeginInit();
+            this.pnlTextos.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picUsername)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPassword)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBanner
+            // 
+            this.picBanner.Image = ((System.Drawing.Image)(resources.GetObject("picBanner.Image")));
+            this.picBanner.Location = new System.Drawing.Point(45, 12);
+            this.picBanner.Name = "picBanner";
+            this.picBanner.Size = new System.Drawing.Size(801, 204);
+            this.picBanner.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBanner.TabIndex = 0;
+            this.picBanner.TabStop = false;
+            // 
+            // pnlTextos
+            // 
+            this.pnlTextos.Controls.Add(this.btnCuentasGoogle);
+            this.pnlTextos.Controls.Add(this.label4);
+            this.pnlTextos.Controls.Add(this.label3);
+            this.pnlTextos.Controls.Add(this.label2);
+            this.pnlTextos.Controls.Add(this.label1);
+            this.pnlTextos.Location = new System.Drawing.Point(45, 281);
+            this.pnlTextos.Name = "pnlTextos";
+            this.pnlTextos.Size = new System.Drawing.Size(475, 315);
+            this.pnlTextos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 91);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenidos a nuestro Campus Virtual\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(6, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(370, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cookies must be enabled in your browser";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(8, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(377, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Login using your account on:\r\n";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.chkUsername);
+            this.panel1.Controls.Add(this.picPassword);
+            this.panel1.Controls.Add(this.picUsername);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(526, 281);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(320, 315);
+            this.panel1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(301, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "__________________________________________";
+            // 
+            // btnCuentasGoogle
+            // 
+            this.btnCuentasGoogle.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnCuentasGoogle.Location = new System.Drawing.Point(14, 234);
+            this.btnCuentasGoogle.Name = "btnCuentasGoogle";
+            this.btnCuentasGoogle.Size = new System.Drawing.Size(263, 41);
+            this.btnCuentasGoogle.TabIndex = 4;
+            this.btnCuentasGoogle.Text = "Cuentas Google UCA";
+            this.btnCuentasGoogle.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(284, 29);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Already have an account?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Tag = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(54, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(135, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // picUsername
+            // 
+            this.picUsername.Image = ((System.Drawing.Image)(resources.GetObject("picUsername.Image")));
+            this.picUsername.Location = new System.Drawing.Point(8, 66);
+            this.picUsername.Name = "picUsername";
+            this.picUsername.Size = new System.Drawing.Size(40, 40);
+            this.picUsername.TabIndex = 3;
+            this.picUsername.TabStop = false;
+            // 
+            // picPassword
+            // 
+            this.picPassword.Image = ((System.Drawing.Image)(resources.GetObject("picPassword.Image")));
+            this.picPassword.Location = new System.Drawing.Point(8, 106);
+            this.picPassword.Name = "picPassword";
+            this.picPassword.Size = new System.Drawing.Size(40, 40);
+            this.picPassword.TabIndex = 4;
+            this.picPassword.TabStop = false;
+            // 
+            // chkUsername
+            // 
+            this.chkUsername.AutoSize = true;
+            this.chkUsername.Location = new System.Drawing.Point(29, 152);
+            this.chkUsername.Name = "chkUsername";
+            this.chkUsername.Size = new System.Drawing.Size(160, 20);
+            this.chkUsername.TabIndex = 5;
+            this.chkUsername.Text = "Remember username\r\n";
+            this.chkUsername.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(20, 206);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(279, 41);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 658);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlTextos);
+            this.Controls.Add(this.picBanner);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ecampus";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBanner)).EndInit();
+            this.pnlTextos.ResumeLayout(false);
+            this.pnlTextos.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picUsername)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPassword)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBanner;
+        private System.Windows.Forms.Panel pnlTextos;
+        private System.Windows.Forms.Button btnCuentasGoogle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picPassword;
+        private System.Windows.Forms.PictureBox picUsername;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox chkUsername;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ tallerCP4/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tallerCP4
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void nintendoSwToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pnlTextos.BackColor = Color.FromArgb(0, 61, 112);
+        }
+    }
+}
